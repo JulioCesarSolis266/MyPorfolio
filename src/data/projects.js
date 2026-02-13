0fd552e3329d7083bf8389e0b967405158b4f029
Julio César Solis <juliocsolis08@hotmail.com>
2026-02-13 20:31:14 -0300

    Fix image names

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@
       title: "Gestor de turnos",
       description:
         "Sistema de gestión de turnos con autenticación y panel de administración.",
-      img: "src/assets/shiftManagementimg.jpg",
+      img: "src/assets/shiftManagementImg.jpg",
       stack: ["React", "Node", "Express", "PostgreSQL"],
       urlDeploy: "https://manicuria-frontend.vercel.app/",
       urlRepoBackend: "https://github.com/JulioCesarSolis266/manicuria-backend",
@@ -14,7 +14,7 @@
       id:2,
       title: "Backend de finanzas personales",
       description: "API REST para gestión de ingresos y gastos.",
-      img: "src/assets/financeManagerimg.jpg",
+      img: "src/assets/financeManagerImg.jpg",
       stack: ["NestJS", "TypeScript", "PostgreSQL"],
       urlDeploy: null,
       urlRepoBackend: "https://github.com/JulioCesarSolis266/personal-finance-backend"
