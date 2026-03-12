dac9ac0282dbc5e8b3a303b0df1c9c95466bbfce
Julio César Solis <juliocsolis08@hotmail.com>
2026-03-12 20:53:15 -0300

    remove seniority

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
             >
               I’m <span className="font-bold">Julio César Solis</span>
               <span className="block mt-2 text-lg sm:text-xl md:text-2xl font-normal">
-                Junior Full-Stack Developer (React.js + Node.js)
+                Full-Stack Developer (React.js + Node.js)
               </span>
             </h2>
 
